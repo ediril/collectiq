dec202c185bb49037d503633836d73df694376d3
Emrah Diril <emrah@diril.org>
2025-09-02 02:21:37 -0400

    remove ref to banalytiq

--- component/endpoint.php
+++ component/endpoint.php
@@ -2,13 +2,6 @@
 
 require_once __DIR__ . '/WaitlistComponent.php';
 
-// Check if banalytiq exists and include it
-$banalytiqPath = __DIR__ . '/../../banalytiq/banalytiq.php';
-if (file_exists($banalytiqPath)) {
-    require_once $banalytiqPath;
-    record_visit();
-}
-
 // Initialize waitlist component
 $waitlist = new WaitlistComponent();
 
